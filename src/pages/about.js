08f73e3d9d8fb4a70c686f9d04cb396360471f7c
Jim <jpstrategy_clr@outlook.com>
2020-04-16 11:27:51 -0400

    Adding graphql

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import Header from '../components/header'
-import Footer from '../components/footer'
+import Layout from '../components/layout'
 
 const AboutPage = () => {
     return (
-        <div>
-            <Header />
+        <Layout>
             <h1>About Me</h1>
             <p>I currently work with entrepreneurs to strengthen their busienss models with digital solutions!</p>
             <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
-            <Footer />
-        </div>
+        </Layout>
     )
 }
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,13 @@
 import React from 'react'
 
-import Header from '../components/header'
-import Footer from '../components/footer'
+import Layout from '../components/layout'
 
 const ContactPage = () => {
     return (
-        <div>
-            <Header />
+        <Layout>
             <h1>Contact Page</h1>
             <p>The best way to contact me is by phone: <a href="860-372-2407">Call Now</a></p>
-            <Footer />
-        </div>
+        </Layout>
     )
 }
 
